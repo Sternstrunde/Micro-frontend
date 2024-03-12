448290323625455518976e7fe36bbd7cef350f78
Sternstrunde <1878856344@qq.com>
2024-03-12 21:49:55 +0800

    🎉 init: 使用koa 创建的服务端

---
+++ build/run.js
@@ -0,0 +1,22 @@
+const childProcess = require("node:child_process");
+const path = require("path");
+
+// 启动所有项目
+
+const filePath = {
+  vue2: path.join(__dirname, "../vue2"),
+  vue3: path.join(__dirname, "../vue3"),
+  react18: path.join(__dirname, "../react18"),
+};
+console.log(filePath);
+// cd 子应用的目录 npm start 启动项目
+function runChild() {
+  Object.values(filePath).forEach((item) => {
+    console.log(item);
+    childProcess.spawn(`cd ${item} && npm run dev`, {
+      stdio: "inherit",
+      shell: true,
+    });
+  });
+}
+runChild();
