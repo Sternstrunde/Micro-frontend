ac57fb63e844f112f13edf5c3d2d8f1799c5f60c
Sternstrunde <1878856344@qq.com>
2024-03-13 15:00:10 +0800

    ✨ feat: 改造vue2成微前端

--- vue2/src/main.js
+++ vue2/src/main.js
@@ -3,7 +3,28 @@ import App from "./App.vue";
 import { router } from "@/router/index";
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  render: (h) => h(App),
-}).$mount("#app");
+let instance = null;
+const render = () => {
+  instance = new Vue({
+    router,
+    render: (h) => h(App),
+  }).$mount("#app");
+};
+
+if (!window.__MICRO_WEB__) {
+  render();
+}
+
+// 开始加载结构
+export const bootstrap = () => {
+  console.log("开始加载");
+};
+
+export const mount = () => {
+  render();
+  console.log("渲染成功");
+};
+
+export const unmount = () => {
+  console.log("卸载", instance);
+};
--- vue2/vue.config.js
+++ vue2/vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
+const { name } = require("./package");
 const path = require("path");
 function resolve(dir) {
   return path.join(__dirname, dir);
@@ -13,20 +14,21 @@ module.exports = defineConfig({
   filenameHashing: true, // 打包出来的文件，会带有hash信息
   publicPath: "http://localhost:9004",
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
     hot: false,
-    disableHostCheck: true,
     port,
     headers: {
       "Access-Control-Allow-Origin": "*", // 本地服务的跨域内容
     },
   },
   configureWebpack: {
+    resolve: {
+      alias: {
+        "@": resolve("src"),
+      },
+    },
     output: {
       libraryTarget: "umd",
-      filename: "vue2.js",
       library: "vue2",
-      jsonpFuncion: `webpackJsonp_${name}`,
     },
   },
 });
