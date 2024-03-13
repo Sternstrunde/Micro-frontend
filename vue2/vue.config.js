ba9ba12bc2c4690772d5a9f0d85c75662b02cf86
Sternstrunde <1878856344@qq.com>
2024-03-13 09:36:15 +0800

    feat:配置路由

--- vue2/vue.config.js
+++ vue2/vue.config.js
@@ -1,7 +1,32 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+const port = 9004;
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  css:{
-    
-  }
-})
+  outputDir: "dist",
+  assetsDir: "static",
+  filenameHashing: true, // 打包出来的文件，会带有hash信息
+  publicPath: "http://localhost:9004",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    hot: false,
+    disableHostCheck: true,
+    port,
+    headers: {
+      "Access-Control-Allow-Origin": "*", // 本地服务的跨域内容
+    },
+  },
+  configureWebpack: {
+    output: {
+      libraryTarget: "umd",
+      filename: "vue2.js",
+      library: "vue2",
+      jsonpFuncion: `webpackJsonp_${name}`,
+    },
+  },
+});
